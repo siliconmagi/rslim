e03b263734ae4ee9299d6e7c45ec9d7a2accdb21
siliconmagi <siliconmagi@yandex.com>
2016-09-18 21:28:37 -0700

    component

---
+++ client/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+const Main = React.createClass({
+    render() {
+        return ( < div > < h1 > <Link to="/">rbrain</Link> < /h1>
+          < /div >)
+    }
+})
+
+export default Main;
--- client/rbrain.js
+++ client/rbrain.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-import {
-    render
-} from 'react-dom';
+import {render} from 'react-dom';
 
+// Import css
 import css from './styles/style.styl';
 
-render( < p > hello < /p>, document.getElementById('root'));
+// Import Components
+import Main from './components/Main';
+render(
+    <Main/>, document.getElementById('root'));
